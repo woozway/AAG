f8ffbb69c96c1865042bd17b7ce1be724d7fb70a
woozway <22060468@zju.edu.cn>
2026-06-21 19:25:25 +0800

    -

--- 0x00 基本算法/0x07 贪心/AcWing114.cpp
+++ 0x00 基本算法/0x07 贪心/AcWing114.cpp
@@ -3,44 +3,38 @@ using namespace std;
 typedef pair<int, int> PII;
 const int N = 1010;
 int n;
-PII p[N];
+PII p[N]; // pair<存 a * b (用于排序)，存 a (左手数字，用于累乘)>
 
-vector<int> mul(vector<int>a, int b) {
-  vector<int> c;
-  int t = 0;
-  for (int i = 0; i < a.size(); i ++ ) {
-    t += a[i] * b;
-    c.push_back(t % 10);
+vector<int> mul(vector<int> &A, int b) {
+  vector<int> C;
+  for (int i = 0, t = 0; i < A.size() || t; i ++ ) {
+    if (i < A.size()) t += A[i] * b;
+    C.push_back(t % 10);
     t /= 10;
   }
-  while (t) {
-    c.push_back(t % 10);
-    t /= 10;
-  }
-  return c;
+  while (C.size() > 1 && C.back() == 0) C.pop_back();
+  return C;
 }
 
-vector<int> div(vector<int>a, int b) {
-  vector<int> c;
-  bool is_first = true;
-  for (int i = a.size() - 1, t = 0; i >= 0; i -- ) {
-    t = t * 10 + a[i];
-    int x = t / b;
-    if (!is_first || x) {
-      is_first = false;
-      c.push_back(x);
-    }
-    t %= b;
+vector<int> div(vector<int> &A, int b, int &r) {
+  vector<int> C;
+  r = 0;
+  for (int i = A.size() - 1; i >= 0; i -- ) {
+    r = r * 10 + A[i];
+    C.push_back(r / b);
+    r %= b;
   }
-  reverse(c.begin(), c.end());
-  return c;
+
+  reverse(C.begin(), C.end());
+  while (C.size() > 1 && C.back() == 0) C.pop_back();
+  return C;
 }
 
-vector<int> max_vec(vector<int> a, vector<int> b) {
-  if (a.size() > b.size()) return a;
-  if (a.size() < b.size()) return b;
-  if (vector<int>(a.rbegin(), a.rend()) > vector<int>(b.rbegin(), b.rend())) return a;
-  return b;
+vector<int> max_vec(vector<int> A, vector<int> B) { // 比较两个高精度数字大小，返回较大的
+  if (A.size() > B.size()) return A;
+  if (A.size() < B.size()) return B;
+  if (vector<int>(A.rbegin(), A.rend()) > vector<int>(B.rbegin(), B.rend())) return A;
+  return B;
 }
 
 int main() {
@@ -50,12 +44,13 @@ int main() {
     cin >> a >> b;
     p[i] = {a * b, a};
   }
-  sort(p + 1, p + n + 1);
+  sort(p + 1, p + n + 1); // 按 a * b 的值对大臣进行排序（国王 p[0] 不参与）
 
-  vector<int> product(1, 1);
-  vector<int> res(1, 0);
+  vector<int> product(1, 1); // product 用于记录当前排在前面所有人的左手数字之积
+  vector<int> res(1, 0); // res 用于记录全场最大奖赏值，初值 0
+  int r;
   for (int i = 0; i <= n; i ++ ) {
-    if (i) res = max_vec(res, div(product, p[i].first / p[i].second));
+    if (i) res = max_vec(res, div(product, p[i].first / p[i].second, r));
     product = mul(product, p[i].second);
   }
 
--- 0x00 基本算法/0x07 贪心/AcWing115.cpp
+++ 0x00 基本算法/0x07 贪心/AcWing115.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 const int N = 1010;
 int n, root;
+// p：父节点的编号；s：当前这个“等效节点”包含了几个原始节点；
+// v：当前这个“等效节点”包含的所有原始节点的权值总和；avg：平均权值 = v / s，贪心的核心依据
 struct Node {
   int p, s, v;
   double avg;
 } nodes[N];
 
-int find() {
+int find() { // 寻找当前树中（除根节点外）平均权值 (avg) 最大的节点
   double avg = 0;
   int res = -1;
   for (int i = 1; i <= n; i ++ )
@@ -18,6 +20,7 @@ int find() {
   return res;
 }
 
+// 先算基础，再算相对延误，以避开复杂的全排序列计算
 int main() {
   cin >> n >> root;
   int ans = 0;
@@ -25,6 +28,8 @@ int main() {
     cin >> nodes[i].v;
     nodes[i].avg = nodes[i].v;
     nodes[i].s = 1;
+    // 假设所有节点都在第 1 个时刻被染色，那么基础代价就是所有节点的权值之和
+    // 后续如果某个节点被推迟了，我们只需要加上“推迟的时间差 * 该节点的权值”即可
     ans += nodes[i].v;
   }
   for (int i = 0; i < n - 1; i ++ ) {
@@ -33,14 +38,22 @@ int main() {
     nodes[b].p = a;
   }
 
+  // 贪心合并，总共需要合并 n - 1 次（把所有非根节点最终都合并到根节点上）
   for (int i = 0; i < n - 1; i ++ ) {
     int p = find();
     int father = nodes[p].p;
+    // 根据贪心策略，一旦 father 被染色，我们应该【立刻】染 p
+    // 这意味着，p 里面包含的节点，其实际染色时间，等于 father 里面包含的节点全部染完后的时间
+    // 换句话说，p 里面的所有节点，被 father 里面的节点“向后排挤”了 father.s 个时间单位
+    // 增加的代价 = 延迟的时间 (father.s) * 被延迟的权值和 (nodes[p].v)
     ans += nodes[p].v * nodes[father].s;
-    nodes[p].avg = -1;
+    nodes[p].avg = -1; // p 已经被合并了，把它的 avg 设为 -1，防止以后再被 find() 找到
+
+    // 将 p 的所有子节点重新认 father 为新的父节点 (相当于把 p 从树中压扁了)
     for (int j = 1; j <= n; j ++ )
       if (nodes[j].p == p)
         nodes[j].p = father;
+    // 更新父节点 father 的状态 (把 p 融进 father 里)
     nodes[father].v += nodes[p].v;
     nodes[father].s += nodes[p].s;
     nodes[father].avg = (double)nodes[father].v / nodes[father].s;
