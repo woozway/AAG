84488d13512b4806bea6835bf5070c00289c89f2
woozway <22060468@zju.edu.cn>
2026-07-26 18:45:07 +0800

    -

--- 0x10 基本数据结构/0x12 队列/AcWing135.cpp
+++ 0x10 基本数据结构/0x12 队列/AcWing135.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
-const int N = 300006;
-const ll INF = 0x3f3f3f3f3f3f3f3f;
-int n, m, q[N];
-ll s[N], ans = -INF;
+typedef long long LL;
+const int N = 3e5 + 10;
+int n, m;
+LL s[N];
+int q[N]; // q 数组模拟双端单调队列，用来存放前缀和的下标
 
 int main() {
-  cin >> n >> m;
-  s[0] = 0; // 前缀和s[j]-s[i-1]表示a[i~j]的和
-  for (int i=1; i<=n; i++) {
-    cin >> s[i];
-    s[i] += s[i-1];
+  scanf("%d%d", &n, &m);
+  for (int i = 1; i <= n; i ++ ) {
+    scanf("%lld", &s[i]);
+    s[i] += s[i - 1];
   }
-  int l = 0, r = 0; // 初始化队列q时，默认加入第一个元素s[0]
-  q[l] = 0; // q队列维护m+1个数的下标集合，因为前缀和的性质要多+1
-  for (int i=1; i<=n; i++) { // l<=r表示队列q非空
-    while (l<=r && q[l]<=i-m-1) l++;
-    // 注意这里要在新下标i入队之前计算ans，因为入队前后q[l]可能改变
-    if (l <= r) ans = max(ans, s[i] - s[q[l]]); // 此时q队列单调
-    while (l<=r && s[q[r]]>=s[i]) r--; // O(1)时间找最值
-    q[++r] = i; // 新元素入队
+
+  LL res = LLONG_MIN;
+  int hh = 0, tt = 0; // 初始时 q[0] = 0, 下标 0 代表前缀和 s[0] = 0，初始状态
+  for (int i = 1; i <= n; i ++ ) {
+    if (q[hh] < i - m) hh ++ ; // 检查队头是否滑出窗口，合法范围：[i - m, i - 1]
+    res = max(res, s[i] - s[q[hh]]);
+    while (hh <= tt && s[q[tt]] >= s[i]) tt -- ;
+    q[ ++ tt] = i;
   }
-  cout << ans << endl;
+
+  printf("%lld\n", res);
   return 0;
 }
--- 0x10 基本数据结构/0x12 队列/AcWing135.py
+++ 0x10 基本数据结构/0x12 队列/AcWing135.py
@@ -1,18 +1,33 @@
-from math import inf
+import sys
+from collections import deque
 
-N = 300006
-q, s = [0] * N, [0] * N
 def main():
-    n, m = [int(x) for x in input().split()]
-    s[1 : n + 1] = [int(x) for x in input().split()]
-    for i in range(1, n + 1): s[i] += s[i - 1]
-    ans, l, r = -inf, 1, 1
-    q[l] = 0
-    for i in range(1, n + 1):
-        while l <= r and q[l] < i - m: l += 1
-        ans = max(ans, s[i] - s[q[l]])
-        while l <= r and s[q[r]] >= s[i]: r -= 1
-        r += 1; q[r] = i
-    print(ans)
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+        
+    n = int(input_data[0])
+    m = int(input_data[1])
     
-main()
+    s = [0] * (n + 1)
+    for i in range(1, n + 1):
+        s[i] = s[i - 1] + int(input_data[i + 1])
+        
+    res = -float('inf')
+    q = deque([0])
+    
+    for i in range(1, n + 1):
+        if q and q[0] < i - m:
+            q.popleft()
+            
+        res = max(res, s[i] - s[q[0]])
+        
+        while q and s[q[-1]] >= s[i]:
+            q.pop()
+            
+        q.append(i)
+        
+    print(res)
+
+if __name__ == '__main__':
+    main()
