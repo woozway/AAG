e9ea6eb5cee6c9bd59d70bfa546c88d091d30efb
woozway <22060468@zju.edu.cn>
2026-08-30 10:39:12 +0800

    -

--- 0x10 基本数据结构/习题/AcWing150.cpp
+++ 0x10 基本数据结构/习题/AcWing150.cpp
@@ -1,53 +1,25 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  string s;
-  cin >> s;
-  stack<int> stk; // 存左括号的下标
-  int ans = 0;
-  for (int i=0; i<s.size(); i++) {
-    char c = s[i];
-    if (stk.empty()) stk.push(i);
-    else {
-      char t = s[stk.top()];
-      if (c==')'&&t=='(' || c==']'&&t=='[' || c=='}'&&t=='{') stk.pop();
-      else stk.push(i);
-    }
-    // 若stk非空，栈顶为第一个非匹配下标，i-stk.top()为中间已匹配弹出段的长度
-    if (!stk.empty()) ans = max(ans, i - stk.top());
-    else ans = max(ans, i + 1); // i开始的字符串中的括号完全匹配
+  string str;
+  cin >> str;
+  stack<int> stk;
+
+  int res = 0;
+  for (int i = 0; i < str.size(); i ++ ) {
+    char c = str[i];
+    if (c == ')' && stk.size() && str[stk.top()] == '(') stk.pop();
+    else if (c == ']' && stk.size() && str[stk.top()] == '[') stk.pop();
+    else if (c == '}' && stk.size() && str[stk.top()] == '{') stk.pop();
+    else stk.push(i);
+
+    // 如果栈不空，那么从 stk.top() + 1 到 i 这一段，一定是一个合法括号子串
+    // 如果栈为空，说明合法的长度就是从整个字符串的开头 0 到当前位置 i
+    if (stk.size()) res = max(res, i - stk.top());
+    else res = max(res, i + 1);
   }
-  cout << ans << endl;
+
+  cout << res << endl;
   return 0;
 }
-
-// #include <iostream>
-// #include <cstring>
-// using namespace std;
-// int n, p, ans, history_match[100010], pos[100010];
-// // f中存放期待匹配的括号，p表示其个数，pos[p]表示第p个左括号的下标
-// char s[100010], f[100010];
-// int main() {
-//   scanf("%s", s);
-//   n = strlen(s), p = 0;
-//   for (int i=0; i<n; i++) {
-//     if (s[i] == '(') f[++p] = ')', pos[p] = i;
-//     else if (s[i] == '[') f[++p] = ']', pos[p] = i;
-//     else if (s[i] == '{') f[++p] = '}', pos[p] = i;
-//     else {
-//       if (p && s[i]==f[p]) { // 发生一次括号匹配
-//         int cur_match = i - pos[p] + 1;
-//         p--;
-//         ans = max(ans, history_match[p] + cur_match);
-//         history_match[p] += cur_match;
-//       }
-//       else { // 以i开始的括号序列不是美观序列
-//         for (int j=0; j<=p; j++) history_match[j] = 0;
-//         p = 0;
-//       }
-//     }
-//   }
-//   cout << ans << endl;
-// }
--- 0x10 基本数据结构/习题/AcWing150.py
+++ 0x10 基本数据结构/习题/AcWing150.py
@@ -1,16 +1,26 @@
-def main():
-    s = input()
-    stk = []
-    ans = 0
-    for i in range(len(s)):
-        c = s[i]
-        if not stk: stk.append(i)
-        else:
-            t = s[stk[-1]]
-            if c==')' and t=='(' or c==']' and t=='[' or c=='}' and t=='{': stk.pop()
-            else: stk.append(i)
-        if stk: ans = max(ans, i - stk[-1])
-        else: ans = max(ans, i + 1)
-    print(ans)
+import sys
 
-main()
+def main():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+        
+    s = input_data[0]
+    stk = []
+    res = 0
+    pairs = {')': '(', ']': '[', '}': '{'}
+    
+    for i, c in enumerate(s):
+        if stk and c in pairs and s[stk[-1]] == pairs[c]:
+            stk.pop()
+        else:
+            stk.append(i)
+            
+        curr = i - stk[-1] if stk else i + 1
+        if curr > res:
+            res = curr
+            
+    print(res)
+
+if __name__ == '__main__':
+    main()
